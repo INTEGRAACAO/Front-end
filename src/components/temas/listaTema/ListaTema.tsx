3db7ea9af880bc4d6845d9657243fadc4e093e7c
Fernando Hemmel <fernando_hs123@outlook.com>
2022-09-14 10:51:05 -0300

    Criando componente Lista Tema

---
+++ src/components/temas/listaTema/ListaTema.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Card, CardActions, CardContent, Button, Typography } from "@material-ui/core";
+import { Box } from '@mui/material'
+import {Navigate} from 'react-router-dom';
+import Tema from '../../../models/Tema'
+import './ListaTema.css';
+import { busca } from '../../../services/Service';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
+
+
+function ListaTema() {
+  const [temas, setTemas] = useState<Tema[]>([])
+  let navigate = useNavigate();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+
+  useEffect(()=> {
+    if(token == ''){
+        toast.error('Você precisa estar logado', {
+            position: "top-right",
+            autoClose: 2000, // fecha depois de 2s
+            hideProgressBar: false,
+            closeOnClick: true, 
+            pauseOnHover: false, // pausa o tempo com o mouse
+            draggable: false, // não pode mover
+            theme: "colored", 
+            progress: undefined,
+        })    
+      navigate("/login")
+    }
+  }, [token])
+
+  async function getTema(){
+    await busca("/temas", setTemas, {
+      headers: {
+        'Authorization': token
+      } 
+    })
+  }
+
+  useEffect(()=>{
+    getTema()
+  }, [temas.length])
+
+    return (
+      <>
+      { 
+        temas.map(tema => (
+        <Box m={2} >
+          <Card variant="outlined">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Tema
+              </Typography>
+              <Typography variant="h5" component="h2">
+                {tema.tema}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Box display="flex" justifyContent="center" mb={1.5} >
+  
+                <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                  <Box mx={1}>
+                    <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                      atualizar
+                    </Button>
+                  </Box>
+                </Link>
+                <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                  <Box mx={1}>
+                    <Button variant="contained" size='small' color="secondary">
+                      deletar
+                    </Button>
+                  </Box>
+                </Link>
+              </Box>
+            </CardActions>
+          </Card>
+        </Box>
+        )) 
+      }
+      </>
+    );
+  }
+  
+  
+  export default ListaTema;
