325eb2246e8136b771c46dc0ddcdb0b95fa5eb96
Cleiton-Guilherme <Cleiton.guilherme@yahoo.com.br>
2022-09-15 15:44:14 -0300

    Estilização Da Página Login

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CadastroProjeto from './components/projetos/CadastroProjeto/CadastroProje
 import CadastroUsuario from './paginas/CadastroUsuario/CadastroUsuario';
 import Contato from './paginas/contato/Contato';
 import Footer from './components/static/footer/Footer';
-import Home from'./paginas/Home/Home';
+import Home from './paginas/Home/Home';
 import Login from './paginas/Login/Login';
 import Navbar from './components/static/navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -15,7 +15,7 @@ import store from './store/store';
 import CadastroTema from './components/temas/cadastroTema/CadastroTema';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import ListaTema from './components/temas/listaTema/ListaTema';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 
 
@@ -23,31 +23,31 @@ function App() {
 
   return (
     <Provider store={store}>
-      <ToastContainer/>
-    <Router>
-    
-      <Navbar />
-      
-      <div style={{ minHeight: '100vh' }}>
-        <Routes> 
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/contato" element={<Contato/>}/>
-          <Route path='/cadastroUsuario' element={<CadastroUsuario/>}/>
-          <Route path='/formularioProjetos' element={<CadastroProjeto/>}/>
-          <Route path='/formularioProjetos/:id' element={<CadastroProjeto/>}/>
-          <Route path='/projetos' element={<ListaProjetos/>}/>
-          <Route path='/deletarProjetos/:id' element={<DeletarProjetos/>}/>
-          <Route path='/temas' element={<ListaTema/>}/>
-          <Route path='/formularioTemas' element={<CadastroTema/>}/>
-          <Route path='/formularioTemas/:id' element={<CadastroTema/>}/>
-          <Route path='/deletarTemas/:id' element={<DeletarTema/>}/>
-          
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
+      <ToastContainer />
+      <Router>
+
+        <Navbar />
+
+        <div style={{ minHeight: '100vh' }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/contato" element={<Contato />} />
+            <Route path='/cadastroUsuario' element={<CadastroUsuario />} />
+            <Route path='/formularioProjetos' element={<CadastroProjeto />} />
+            <Route path='/formularioProjetos/:id' element={<CadastroProjeto />} />
+            <Route path='/projetos' element={<ListaProjetos />} />
+            <Route path='/deletarProjetos/:id' element={<DeletarProjetos />} />
+            <Route path='/temas' element={<ListaTema />} />
+            <Route path='/formularioTemas' element={<CadastroTema />} />
+            <Route path='/formularioTemas/:id' element={<CadastroTema />} />
+            <Route path='/deletarTemas/:id' element={<DeletarTema />} />
+
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </Provider>
   );
 }
