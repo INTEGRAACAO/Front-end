2c314a8dccd7f95ceec39e1624bbec82ad703a63
luizavramos <luizavramos30@gmail.com>
2022-09-17 18:39:24 -0300

    Ajustes login

--- src/paginas/Login/Login.tsx
+++ src/paginas/Login/Login.tsx
@@ -85,13 +85,13 @@ function Login() {
 
   return (
 
-    <Grid container direction="row" justifyContent="center" alignItems="center" >
+    <Grid container direction="row" justifyContent="center" alignItems="center" className='bg-login' >
       
       <Grid item xs={2} className="imagem2">
-      <img src="https://i.imgur.com/0Ggm1U0.png" alt="" />
+      <img src="https://i.imgur.com/Zzef03Z.pngyarm" alt="" className='img-login'/>
       </Grid>
       
-      <Grid item xs={10} alignItems="center">
+      <Grid item xs={5} alignItems="center">
       <Box className="card-login">
             <Typography className='card-title-login' variant="h4" align="center">
               Login
@@ -124,6 +124,10 @@ function Login() {
 
           </Box>
       </Grid>
+      <Grid item xs={5} alignItems='center'>
+        <img src="https://i.imgur.com/05EJeab.png" alt="" />
+      </Grid>
+      
           
           
 
