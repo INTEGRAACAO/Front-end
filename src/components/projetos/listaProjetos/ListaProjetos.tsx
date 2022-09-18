1a160e9b5ad9eb9bcfc3fcbd90be73f750f67aba
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2022-09-18 12:33:55 -0300

    ajustes css

--- src/components/projetos/listaProjetos/ListaProjetos.tsx
+++ src/components/projetos/listaProjetos/ListaProjetos.tsx
@@ -7,6 +7,8 @@ import { useSelector } from 'react-redux';
 import { UserState } from '../../../store/tokens/tokensReducer';
 import {toast} from 'react-toastify'
 import Projetos from '../projetos/projetos';
+import CadastroProjeto from '../CadastroProjeto/CadastroProjeto';
+import { Box } from '@mui/material';
 
 function ListaProjetos() {
   const [posts, setPosts] = useState<Projeto[]>([])
@@ -48,6 +50,7 @@ function ListaProjetos() {
 
   return (
     <>
+    <Box> <CadastroProjeto/></Box>
     <section className="projetos">
       {posts.map(post => (
         <Projetos projeto={post}/> 
