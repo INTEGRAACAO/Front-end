7f489a8c4266e881f6e63732522f750bf3cf16b7
Daniel Augusto <daniel.aug.souza@gmail.com>
2022-09-23 10:45:04 -0300

    Ajustes footer

--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -14,13 +14,9 @@ function Footer() {
 
     var footerComponent;
     if (token != "") {
-        footerComponent = <Grid container justifyContent="space-between" >
+        footerComponent = <Grid container >
             <Grid alignItems="center" item xs={12}>
                 <Box className='box1'>
-                    <Box paddingTop={1} alignContent="center">
-                        
-                    </Box>
-
                     <Box>
                         <Typography  align="center" gutterBottom className='textos' >© 2022 Copyright:</Typography>
                         <a target="_blank" href="https://brasil.generation.org" className='text-decoration-none'>
@@ -28,17 +24,18 @@ function Footer() {
                         </a>
                     </Box>
 
-                    <Box display="flex" alignItems="center" justifyContent="center">
+                    <Box id="nossoTrabalho" >
                         <Typography variant="h5" align="center" gutterBottom className='textos'> Veja nosso trabalho </Typography>
-                        <a href="https://github.com/INTEGRAACAO/Back-end" target="_blank">
-                            <GitHubIcon className='redes' />
-                            <Typography variant="subtitle2" gutterBottom className='textos ' align="center">BackEnd</Typography>
-                        </a>
-                        <a href="https://github.com/INTEGRAACAO/Front-end" target="_blank">
-                            <GitHubIcon className='redes' />
-                            <Typography variant="subtitle2" gutterBottom className='textos ' align="center">FrontEnd</Typography>
-                        </a>
-
+                        <div id="nossoRedes">
+                          <a href="https://github.com/INTEGRAACAO/Back-end" target="_blank">
+                              <GitHubIcon className='redes' />
+                              <Typography variant="subtitle2" gutterBottom className='textos ' align="center">BackEnd</Typography>
+                          </a>
+                          <a href="https://github.com/INTEGRAACAO/Front-end" target="_blank">
+                              <GitHubIcon className='redes' />
+                              <Typography variant="subtitle2" gutterBottom className='textos ' align="center">FrontEnd</Typography>
+                          </a>
+                        </div>
                     </Box>
                 </Box>
 
